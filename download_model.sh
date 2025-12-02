e50f067d1ca3aca1d37715f5e5b0edc6218a8dfc
LinYuOu <oulinyu18608447804@163.com>
2025-12-02 18:34:25 +0800

    add model merge and model download

---
+++ download_model.sh
@@ -0,0 +1,4 @@
+python download_hf.py --repo_id Qwen/Qwen3-VL-30B-A3B-Instruct --local_dir ~/share_data/Qwen/Qwen3-VL-30B-A3B-Instruct
+python download_hf.py --repo_id Qwen/Qwen3-VL-8B-Instruct --local_dir ~/share_data/Qwen/Qwen3-VL-8B-Instruct
+python download_hf.py --repo_id Qwen/Qwen3-VL-4B-Instruct --local_dir ~/share_data/Qwen/Qwen3-VL-4B-Instruct
+# python download_hf.py --repo_id zai-org/CogVideoX-5b --local_dir 
